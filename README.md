234958441fbb3ba9df66d76a77dfe225e4bbdcd6
Ismanol García <dev@ismanolgarcia.com>
2023-01-10 21:20:59 -0400

    feat: display web page created

---
+++ README.md
@@ -0,0 +1,30 @@
+# Web projects to practice
+
+#### Personal challenges to create web projects in html, css and vanilla js
+
+At the beginning of the year I decided that I should practice my web development skills more.
+
+That's why I decided to create several projects with html, css and javascript. Starting from the simplest projects to the most complex ones.
+
+
+### The practical exercises are obtained from the following pages:
+
+- [Frontend Mentor](https://www.frontendmentor.io/challenges)
+
+- [FreeCodeCamp](https://www.freecodecamp.org/espanol/news/40-proyectos-de-javascript-para-principiantes-ideas-faciles-para-empezar-a-codificar-en-js/)
+
+
+
+### Links
+- Solution URL: [here](https://github.com/ismanolgarcia/web-projects-to-practice)
+
+- Live Site URL: [here]()
+
+### Built with
+- Semantic HTML5 markup
+- CSS
+- Vanilla JS
+
+
+### Author
+- Ismanol Garcia 
---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+import { projects } from '../../data/projects.js';
+
+const root = document.getElementById('root');
+
+projects.map((project, index) => {
+  const wrap = document.createElement('div');
+  wrap.classList = 'projects';
+
+  wrap.innerHTML = `
+  <img src="/${project.name}/design/desktop-design.png" alt="${project.name}" class="projects__img" />
+  <p class="projects__title">${1 + index} ${project.name}</p>
+  <div class="container__link">
+    <a href="/${project.name}/index.html" target="_blank" rel="noopener noreferrer" class="projects__link" >
+      <img src="./src/icons/preview-svgrepo-com.svg" alt="web logo" class="projects__link--logo" />
+    </a>
+    <a href="${project.github}" target="_blank" rel="noopener noreferrer" class="projects__link" >
+      <img src="./src/icons/github-svgrepo-com.svg" alt="Github logo" class="projects__link--logo" />
+    </a>
+  </div>
+  `;
+  root.appendChild(wrap);
+});
