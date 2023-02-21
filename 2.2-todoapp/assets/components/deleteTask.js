b8c9595f4a8dec02fff842ee1ed4087d7bd6aa62
Ismanol García <dev@ismanolgarcia.com>
2023-02-21 16:27:14 -0400

    feat: deployed todoapp

---
+++ 2.2-todoapp/assets/components/deleteTask.js
@@ -0,0 +1,14 @@
+const trashIcon = () => {
+  const i = document.createElement('i');
+  i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+  i.addEventListener('click', deleteTask);
+  return i;
+};
+
+const deleteTask = (event) => {
+  const element = event.target;
+  const parent = event.target.parentElement;
+  parent.remove();
+};
+
+export default trashIcon;
--- data/projects.js
+++ data/projects.js
@@ -1,57 +1,62 @@
 export const projects = [
   {
-    name: "1.1-qr-code-component",
+    name: '1.1-qr-code-component',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.1-qr-code-component",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.1-qr-code-component',
   },
   {
-    name: "1.2-profile-card-component",
+    name: '1.2-profile-card-component',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.2-profile-card-component",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.2-profile-card-component',
   },
   {
-    name: "1.3-column-preview-card",
+    name: '1.3-column-preview-card',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.3-column-preview-card",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.3-column-preview-card',
   },
   {
-    name: "1.4-single-price-grid",
+    name: '1.4-single-price-grid',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.4-single-price-grid",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.4-single-price-grid',
   },
   {
-    name: "1.5-huddle-landing-page-section",
+    name: '1.5-huddle-landing-page-section',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.5-huddle-landing-page-section",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.5-huddle-landing-page-section',
   },
   {
-    name: "1.6-simple-counter",
+    name: '1.6-simple-counter',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.6-simple-counter",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.6-simple-counter',
   },
   {
-    name: "1.7-color-flipper",
+    name: '1.7-color-flipper',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.7-color-flipper",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.7-color-flipper',
   },
   {
-    name: "1.8-signup-form",
+    name: '1.8-signup-form',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.8-signup-form",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.8-signup-form',
   },
   {
-    name: "1.9-base-apparel-coming-soon",
+    name: '1.9-base-apparel-coming-soon',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.9-base-apparel-coming-soon",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/1.9-base-apparel-coming-soon',
   },
   {
-    name: "2.0-gallery-cards",
+    name: '2.0-gallery-cards',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/2.0-gallery-cards",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/2.0-gallery-cards',
   },
   {
-    name: "2.1-web-page-fruits",
+    name: '2.1-web-page-fruits',
     github:
-      "https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/2.1-web-page-fruits",
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/2.1-web-page-fruits',
+  },
+  {
+    name: '2.2-todoapp',
+    github:
+      'https://github.com/ismanolgarcia/web-projects-to-practice/tree/main/2.2-todoapp',
   },
 ];
